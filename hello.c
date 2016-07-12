99ef082524c43cd40ff9e31dc17236a25f766c66
Hu Yong <huyong@huyongs-MacBook-Pro.local>
2016-07-12 14:01:37 +0800

    apue code

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float a,b,c;
+    printf("Input a,b,c:");
+    scanf("%f, %f, %f", &a, &b, &c);
+    printf("Hello world Output\n a = %f b = %f c = %f", a, b, c);
+    printf("\n");
+    return 0;
+}
+    
